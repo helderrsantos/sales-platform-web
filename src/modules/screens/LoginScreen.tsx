9b709bebd9088df20e85f9172737a2c35ade7b0a
helder <helderr_santos@hotmail.com>
2023-10-06 00:30:16 -0300

    feat: create module login

---
+++ src/modules/screens/LoginScreen.tsx
@@ -0,0 +1,9 @@
+import { BackgroundImage } from "../styles/loginScreen.styles"
+
+export const LoginScreen = () => {
+    return (
+        <div>
+            <BackgroundImage src="./background.png" />
+        </div>
+    )
+}
